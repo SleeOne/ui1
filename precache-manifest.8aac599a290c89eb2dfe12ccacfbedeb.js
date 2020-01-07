9dc9e54e294cd92a4d44a7897c00c47a7f6596b3
sith <krevetko253@gmail.com>
2020-01-07 21:27:29 +0300

    Updates

---
+++ precache-manifest.8aac599a290c89eb2dfe12ccacfbedeb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51938593af44576a1c16d5cfff328788",
+    "url": "/ui1/index.html"
+  },
+  {
+    "revision": "e0ce48f958d868e51f55",
+    "url": "/ui1/static/css/main.6e173879.chunk.css"
+  },
+  {
+    "revision": "4ec0d4351a3d6fe534fb",
+    "url": "/ui1/static/js/2.6134cc61.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ui1/static/js/2.6134cc61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0ce48f958d868e51f55",
+    "url": "/ui1/static/js/main.e91dc937.chunk.js"
+  },
+  {
+    "revision": "17a7110a33eb983efa5e",
+    "url": "/ui1/static/js/runtime-main.8422c181.js"
+  },
+  {
+    "revision": "379fdd8ffa5f5e1e3374044d4ec19eb6",
+    "url": "/ui1/static/media/e.379fdd8f.svg"
+  },
+  {
+    "revision": "cd7ab36d7351bbbb82c7dc615e2ca541",
+    "url": "/ui1/static/media/hero.cd7ab36d.png"
+  },
+  {
+    "revision": "f4f58dcdca30f84a1af87f4dbe4f345b",
+    "url": "/ui1/static/media/section2.f4f58dcd.png"
+  },
+  {
+    "revision": "42d5242a2f70032b9853f2ddc9a63692",
+    "url": "/ui1/static/media/section3.42d5242a.png"
+  },
+  {
+    "revision": "e2584c971ff9ca4b874151b14bd9588c",
+    "url": "/ui1/static/media/section4.e2584c97.png"
+  }
+]);
